af19e0966d457c771747283608bb73a53ce07663
buscoma <matias.busco@mercadolibre.com>
2020-04-21 00:28:35 -0300

    counter in login_page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar'
+import Counter from './Components/LoginPage/LoginPage'
 
 function App() {
   let user = {'Name': 'Pablo'};
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Navbar AppName="lenguamaticag1" User={user}></Navbar>
-      Hola Mundo!!!
+      <Counter/>
     </div>
   );
 }
