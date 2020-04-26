861ad206c3bad53bdce2ea1f9a61ed1ba267c4cb
Alejandra <acurbelo@outlook.com>
2020-04-26 15:26:05 -0300

    Creacion de comp. y modif de App.js
    Cree componentes: Ranking list + triple button (lengua + matematica + ranking)
    
    Los agregue en App.js
    
    No pude cambiar la foto de fondo de c/ boton

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import './App.css';
+import TrippleButton from './Components/TripleButton.js';
+import RankingList from './Components/RankingList.js';
+//import TestImg from "./Images/test.svg";
 
 function App() {
   return (
+    
     <div className="App">
-      Hola Mundo!!!
+    
+      <h1> LenguaMatica!</h1>
+
+      <TrippleButton />
+      
+      <h1> Ranking: </h1>
+
+      <RankingList />
+
+      { /* <img src={TestImg} alt='' /> */ }
+
     </div>
   );
 }
-
-
-class Test_comp extends Component {
-  render () {
-    return React.createElement('div', null, 'h1', 'Hi, Ale');
-
-  }
-}
-
 
 export default App;
