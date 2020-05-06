ce6218c9c913670e2b758cb0817157c634e9d1c2
Alejandra <acurbelo@outlook.com>
2020-05-05 21:37:09 -0300

    Palabras perdidas con cards dinamicos

--- src/Containers/PalabrasPerdidas.js
+++ src/Containers/PalabrasPerdidas.js
@@ -5,47 +5,60 @@ import '../css/PalabrasPerdidas/palabrasPerdidas.css';
 import Board from '../Components/Board';
 import Card from '../Components/Card';
 
+const row = [
+    {
+        id: '1',
+        palabra: 'busca',
+        frase_frente:'Mi hermano',
+        frase_atras:'sus gafas'
+    },
+    {
+        id: '2',
+        palabra: 'hizo',
+        frase_frente:'El pintor',
+        frase_atras:'su primera exposición'
+    },
+    {
+        id: '3',
+        palabra: 'gusta',
+        frase_frente:'A la niña le',
+        frase_atras:'ir al jardín'
+    },
+]
+
 function PalabrasPerdidas() {
 
     return (
         <div >
-
             <div className="header">
-                <h1>Palabras perdidas</h1>
-                <p>Completa las frases utilizando las palabras del recuadro</p>
+                <h1> Palabras perdidas </h1>
+                <p> Completa las frases utilizando las palabras del recuadro </p>
             </div>
-
+            
             <div className="row">
-                <div className="column side">
-                
+
+                <div className="column side">                
                     <div className="boxPalabras">
                         <Board id="board-0" className="board">
-                            <Card id="card-2" draggable="true">   <p> hizo </p>   </Card>
-                            <Card id="card-1" draggable="true">   <p> busca</p>   </Card>
+                            {row.map((item) => <Card id={item.id} draggable="true"> <p> {item.palabra} </p> </Card> )}
                         </Board>
                     </div>
                 </div>
-                    
-                <div className="column middle">            
-                    <div className="clearfix">
-                        Mi hermano 
-                        <div className="flexbox">
+                
+                <div className="column middle">
+                    {row.map((item) =>  
+                        <div className="clearfix">
+                            <p> {item.frase_frente}
+                            <div className="flexbox">
                             <Board id="board-1" className="board">
                             </Board>
                         </div>
-                        sus gafas
+                        {item.frase_atras}  </p>                             
                     </div>
-                    
-                    <div className="clearfix">
-                        <div className="divAlign"> El pintor </div>
-                            <div className="flexbox" >
-                                <Board id="board-1" className="board">                
-                                </Board>
-                            </div>
-                        <div className="divAlign"> su primera exposición </div>              
-                    </div>                
+                    )}                    
                 </div>
             </div>
+
         </div>
     );
 }
