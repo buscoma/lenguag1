0d9a34394c380d5b93960274f2a585c0bb65e25f
buscoma <matias.busco@mercadolibre.com>
2020-05-25 23:56:00 -0300

    borre cosas

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,5 @@
 // Librerias
 import React from "react";
-<<<<<<< HEAD
-import Home from "./Containers/Home.js";
-import LandingPage from "./Containers/LandingPage.js";
-import RankingPage from "./Containers/RankingPage.js";
-import LengGameOne from "./Containers/LengGameOne.js";
-import PalabrasPerdidas from "./Containers/PalabrasPerdidas.js";
-import JuegoNumAPalabra from "./Containers/JuegoNumAPalabra";
-import ComprensionLectora from "./Containers/ComprensionLectora";
-=======
 
 // Miscelaneos
 import Home from "./Views/Home";
@@ -25,7 +16,6 @@ import PalabrasPerdidas from "./Views/PalabrasPerdidas";
 import JuegoNumAPalabra from "./Views/JuegoNumAPalabra";
 import BurgerBuilder from "./Views/BurgerBuilder";
 import SecuenciaNumeros from "./Views/SecuenciaNumeros";
->>>>>>> develop
 
 const routes = [
   {
@@ -39,35 +29,14 @@ const routes = [
   {
     key: 2,
     public: false,
-<<<<<<< HEAD
-    name: "LandingPage",
-    path: "/landing_page",
-    exact: true,
-    main: () => <LandingPage />,
-=======
     name: "RankingPage",
     path: "/ranking_page",
     exact: true,
     main: () => <RankingPage />,
->>>>>>> develop
   },
   {
     key: 3,
     public: false,
-<<<<<<< HEAD
-    name: "LengGameOne",
-    path: "/leng_game_one",
-    exact: true,
-    main: () => <LengGameOne />,
-  },
-  {
-    key: 4,
-    public: false,
-    name: "RankingPage",
-    path: "/ranking_page",
-    exact: true,
-    main: () => <RankingPage />,
-=======
     name: "LandingPage",
     path: "/landing_page",
     exact: true,
@@ -81,13 +50,10 @@ const routes = [
     path: "/comprension_lectora",
     exact: true,
     main: () => <ComprensionLectora />,
->>>>>>> develop
   },
   {
     key: 5,
     public: false,
-<<<<<<< HEAD
-=======
     name: "PalabrasCorrectas",
     path: "/palabras_correctas",
     exact: true,
@@ -96,35 +62,20 @@ const routes = [
   {
     key: 6,
     public: false,
->>>>>>> develop
     name: "PalabrasPerdidas",
     path: "/palabras_perdidas",
     exact: true,
     main: () => <PalabrasPerdidas />,
   },
-<<<<<<< HEAD
-  {
-    key: 6,
-=======
 
   {
     key: 7,
->>>>>>> develop
     public: false,
     name: "NumeroAPalabra",
     path: "/numero_a_palabra",
     exact: true,
     main: () => <JuegoNumAPalabra />,
   },
-<<<<<<< HEAD
-  {
-    key: 7,
-    public: true,
-    name: "ComprensionLectora",
-    path: "/comprension_lectora",
-    exact: true,
-    main: () => <ComprensionLectora />,
-=======
 
   {
     key: 8,
@@ -142,7 +93,6 @@ const routes = [
     path: "/secuencia_numeros",
     exact: true,
     main: () => <SecuenciaNumeros />,
->>>>>>> develop
   },
 ];
 
