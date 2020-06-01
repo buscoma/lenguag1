8b414254ba1743cb0dfb295bdd0f77312ca755fc
rapamatiase <mrapaport@euade.uade.ar>
2020-06-01 18:53:33 -0300

    Palabras correctas es muy incorrecto todo

--- src/Views/PalabrasCorrectas/index.js
+++ src/Views/PalabrasCorrectas/index.js
@@ -14,6 +14,12 @@ import Paper from '@material-ui/core/Paper';
 import LayoutGame from '../../Components/Layout/LayaoutContainer';
 import background from './Assets/background.jpg';
 
+//Internas
+import "./Styles/PalabraCorrectasStyle.css";
+
+
+
+
 
 
 class App extends Component {
@@ -39,8 +45,9 @@ class App extends Component {
 		mensaje: "",
 	};
 
-	setShow(state) {
-		this.setState({ show: state })
+	setShow() {
+		let oldState = this.state.show;
+		this.setState({ show: !oldState })
 	}
 
 	onRestart = () => { };
@@ -66,12 +73,13 @@ class App extends Component {
 		if (this.state.text[this.state.position].EsCorrecta === false) {
 			this.setState({ mensaje: 'Esta escrita incorrecto!! ' });
 			this.setState({ right: true });
-			if (this.state.text.length - 1 > this.state.position) {
-				this.setState({ nextWord: true });
-			} else {
-				//this.setState({ mensaje: 'Has ganado!! ' });
-				this.setState({ stateOfGame: "WINNER" });
-			}
+			this.setState({ nextWord: true });
+			/*			if (this.state.text.length - 1 > this.state.position) {
+							
+						} else {
+							//this.setState({ mensaje: 'Has ganado!! ' });
+							this.setState({ stateOfGame: "WINNER" });
+						}*/
 		} else {
 			this.setState({ mensaje: 'Esta escrita correctamente!! ' });
 			this.setState({ wrong: true });
@@ -80,11 +88,19 @@ class App extends Component {
 	}
 
 	playNextWord() {
-		let x = this.state.position;
-		this.setState({ position: x + 1 });
-		this.setState({ nextWord: false });
-		this.setState({ right: false });
-		this.setState({ wrong: false });
+		if (this.state.text.length - 1 > this.state.position) {
+			let x = this.state.position;
+			this.setState({ position: x + 1 });
+			this.setState({ nextWord: false });
+			this.setState({ right: false });
+			this.setState({ wrong: false });
+		} else {
+			this.setState({ stateOfGame: "END" });
+			//this.setShow(true)
+			alert(this.state.stateOfGame);
+			this.setState({ show: true });
+			alert(this.state.show);
+		}
 	}
 
 	render() {
@@ -93,62 +109,74 @@ class App extends Component {
 				backgroundImage={background}
 				title="Palabras Correctas"
 				enunciado="Sos bueno escribiendo. Identifica si la palabra esta escrita correctamente o no."
-				show={this.show}
+				show={this.state.show}
 				setShow={this.setShow}
-				stateOfGame={this.stateOfGame}
+				stateOfGame={this.state.stateOfGame}
 			>
-				<div >
-					<Container fixed>
-						<Grid container spacing={3} style={{padding:"20px"}}>
-							<Grid item xs={12}>
-								<Paper elevation={3} style={{padding:"20px"}}>
-									<Grid container spacing={4}>
-										<Grid item xs={12}>
-											<Typography className="center">Esta palabra esta escrita correctamente?</Typography>
-										</Grid>
-										<Grid item xs={12} style={{textAlign:"center"}}>
-											"{this.state.text[this.state.position].palabra}"
-										</Grid>
-									</Grid>
-								</Paper>
-							</Grid>
-							<Grid item xs={12}>
-								<Collapse in={!this.state.nextWord} style={{ width: "100%" }}>
-									<Grid container spacing={3}>
-										<Grid item xs={12} md={6}>
-											<Button variant="contained" onClick={() => this.checkIfCorrect()} fullWidth>SI </Button>
-										</Grid>
-										<Grid item xs={12} md={6}>
-											<Button variant="contained" onClick={() => this.checkIfInCorrect()} fullWidth>NO </Button>
-										</Grid>
-									</Grid>
-								</Collapse>
-							</Grid>
+					<div >
+						<Container fixed>
+							<Grid container spacing={2}>
+								<Grid item xs={12}>
+									<Paper elevation={3} className="MuiPaper-root" style={{ padding: "20px" }}>
+										<Grid container spacing={3} style={{ padding: "20px" }}>
+											<Grid item xs={12}>
 
-							<Collapse in={this.state.nextWord} style={{ width: "100%" }}>
-								<Grid item xs={12} md={12}>
-									<Button variant="contained" onClick={() => this.playNextWord()} fullWidth> CONTINUAR </Button>
+												<Grid container spacing={4}>
+													<Grid item xs={12}>
+														<Typography className="MuiTypography-body1" variant="body1">Esta palabra esta escrita correctamente?</Typography>
+													</Grid>
+													<Grid item xs={12}  >
+														<Typography variant="h6" classes={{h6 : 'aca'}}> "{this.state.text[this.state.position].palabra}"</Typography>
+
+													</Grid>
+												</Grid>
+
+											</Grid>
+											<Grid item xs={12}>
+												<Grid container spacing={3}>
+													<Grid item xs={12} md={6}>
+														<Button variant="contained" disabled={this.state.nextWord} onClick={() => this.checkIfCorrect()} fullWidth>SI </Button>
+													</Grid>
+													<Grid item xs={12} md={6}>
+														<Button variant="contained" disabled={this.state.nextWord} onClick={() => this.checkIfInCorrect()} fullWidth>NO </Button>
+													</Grid>
+												</Grid>
+											</Grid>
+
+										</Grid>
+									</Paper>
 								</Grid>
-							</Collapse>
-
-							<Grid item xs={12}>
-								<Collapse in={this.state.wrong}>
-									<Alert severity="error" >
-										<AlertTitle>RESPUESTA INCORRECTO</AlertTitle>
-										{this.state.mensaje}
-									</Alert>
-								</Collapse>
-								<Collapse in={this.state.right}>
-									<Alert severity="success" >
-										<AlertTitle>RESPUESTA CORRECTO</AlertTitle>
-										{this.state.mensaje}
-
-									</Alert>
-								</Collapse>
+								<Grid item xs={12}>
+									<Collapse in={this.state.wrong}>
+										<Grid container spacing={3}>
+											<Grid item xs={12} md={12}>
+												<Alert severity="error" >
+													<AlertTitle>RESPUESTA INCORRECTO</AlertTitle>
+													{this.state.mensaje}
+												</Alert>
+											</Grid>
+											<Grid item xs={12} md={12}>
+												<Button variant="contained" onClick={() => this.playNextWord()} fullWidth> VOLVER A JUGAR </Button>
+											</Grid>
+										</Grid>
+									</Collapse>
+									<Collapse in={this.state.right}>
+										<Grid container spacing={3}>
+											<Grid item xs={12} md={12}>
+												<Alert severity="success" >
+													<AlertTitle>RESPUESTA CORRECTO</AlertTitle>
+													{this.state.mensaje}
+												</Alert>
+											</Grid>
+											<Grid item xs={12} md={12}>
+												<Button variant="contained" onClick={() => this.playNextWord()} fullWidth> ESTAS LISTO PARA CONTINUAR </Button>
+											</Grid>
+										</Grid>
+									</Collapse>
+								</Grid>
 							</Grid>
-						</Grid>
-					</Container>
-				</div>
+						</Container>
+					</div>
 			</LayoutGame>
 		);
 	}
