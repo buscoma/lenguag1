0edba93dfce604bd9b39f4a9e572fb5929d95592
Pablo <pdurand@planexware.com>
2020-06-25 16:49:22 -0300

    Subo integración de num a palabra

--- src/Views/JuegoNumAPalabra/Controller/index.js
+++ src/Views/JuegoNumAPalabra/Controller/index.js
@@ -1,65 +1,75 @@
-class JuegoNumAPalabraCtrl {
-  obtenerNivel(dificultad) {
-    switch (dificultad) {
-      case 1:
-        return {
-          Numero: 150,
-          Respuestas: [
-            {
-              Descripcion: "Ciento cincuenta",
-              Correcta: true,
-            },
-            {
-              Descripcion: "Ciento cincuenta y cinco",
-              Correcta: false,
-            },
-            {
-              Descripcion: "Quinientos veintitres",
-              Correcta: false,
-            },
-          ],
-        };
-      case 2:
-        return {
-          Numero: 210,
-          Respuestas: [
-            {
-              Descripcion: "Ciento cincuenta y cinco",
-              Correcta: false,
-            },
-            {
-              Descripcion: "Doscientos diez",
-              Correcta: true,
-            },
-            {
-              Descripcion: "Quinientos veintitres",
-              Correcta: false,
-            },
-          ],
-        };
-      case 3:
-        return {
-          Numero: 1003,
-          Respuestas: [
-            {
-              Descripcion: "Quinientos veintitres",
-              Correcta: false,
-            },
-            {
-              Descripcion: "Mil tres",
-              Correcta: true,
-            },
-            {
-              Descripcion: "Ciento cincuenta y cinco",
-              Correcta: false,
-            },
-          ],
-        };
-        default:
-          console.log("Caso defualt en el switch del controler de JuegoNUmApalabra");
-        break;
-    }
-  }
-}
+import {authFetch} from "../../../AuthProvider";
 
-export default new JuegoNumAPalabraCtrl();
+export const obtenerNivel = (dificultad) => {
+  
+    return authFetch("https://backendlenguamaticag1.herokuapp.com/api/games/juegoNumAPalabra?nivel=" + dificultad)
+    .then(res=>res.json())
+    .then((result)=> {return result.data})
+    .catch((error)=>{console.log("error", error)});
+
+}
+// class JuegoNumAPalabraCtrl {
+//   obtenerNivel(dificultad) {
+//     switch (dificultad) {
+//       case 1:
+//         return {
+//           Numero: 150,
+//           Respuestas: [
+//             {
+//               Descripcion: "Ciento cincuenta",
+//               Correcta: true,
+//             },
+//             {
+//               Descripcion: "Ciento cincuenta y cinco",
+//               Correcta: false,
+//             },
+//             {
+//               Descripcion: "Quinientos veintitres",
+//               Correcta: false,
+//             },
+//           ],
+//         };
+//       case 2:
+//         return {
+//           Numero: 210,
+//           Respuestas: [
+//             {
+//               Descripcion: "Ciento cincuenta y cinco",
+//               Correcta: false,
+//             },
+//             {
+//               Descripcion: "Doscientos diez",
+//               Correcta: true,
+//             },
+//             {
+//               Descripcion: "Quinientos veintitres",
+//               Correcta: false,
+//             },
+//           ],
+//         };
+//       case 3:
+//         return {
+//           Numero: 1003,
+//           Respuestas: [
+//             {
+//               Descripcion: "Quinientos veintitres",
+//               Correcta: false,
+//             },
+//             {
+//               Descripcion: "Mil tres",
+//               Correcta: true,
+//             },
+//             {
+//               Descripcion: "Ciento cincuenta y cinco",
+//               Correcta: false,
+//             },
+//           ],
+//         };
+//         default:
+//           console.log("Caso defualt en el switch del controler de JuegoNUmApalabra");
+//         break;
+//     }
+//   }
+// }
+
+// export default new JuegoNumAPalabraCtrl();
