c0ccdd6b5890ac81d8da46a3a631c3faee3e3aad
pablodnd <pablogaston1989@gmail.com>
2020-06-30 22:14:45 -0300

    integro backend de compresion lectora

--- src/Views/ComprensionLectora/Controller/index.js
+++ src/Views/ComprensionLectora/Controller/index.js
@@ -1,66 +1,15 @@
-class ComprensionLectoraController {
-  obtenerNivel = (dificultad) => {
-    switch (dificultad) {
-      case 1:
-        return {
-          Pregunta:
-            "¿La mama de Julia tiene cinco hijas Papa, Pepa, Pipa y Popa. Como se llama la ultima hija?",
-          Respuestas: [
-            {
-              Descripcion: "Pupa",
-              Correcta: false,
-            },
-            {
-              Descripcion: "Julia",
-              Correcta: true,
-            },
-            {
-              Descripcion: "Pepa",
-              Correcta: false,
-            },
-          ],
-        };
-      case 2:
-        return {
-          Pregunta: "¿De que color ese el caballo blanco de San Martin?",
-          Respuestas: [
-            {
-              Descripcion: "Blanco",
-              Correcta: true,
-            },
-            {
-              Descripcion: "Negro",
-              Correcta: false,
-            },
-            {
-              Descripcion: "Azul",
-              Correcta: false,
-            },
-          ],
-        };
-      case 3:
-        return {
-          Pregunta: "¿Que pesa mas un Kg de acero o un Kg de algodón ?",
-          Respuestas: [
-            {
-              Descripcion: "Un Kg de acero",
-              Correcta: false,
-            },
-            {
-              Descripcion: "Un Kg de plumas",
-              Correcta: false,
-            },
-            {
-              Descripcion: "Ambos pesan lo mismo",
-              Correcta: true,
-            },
-          ],
-        };
-        default:
-          console.log("Caso defualt en el switch del controler de Compresion lectora COntroller");
-        break;
-    }
-  };
-}
+import { authFetch } from "../../../AuthProvider";
 
-export default new ComprensionLectoraController();
+export const obtenerNivel = (dificultad) => {
+    return authFetch(
+        "https://backendlenguamaticag1.herokuapp.com/api/games/comprensionLectora?nivel=" +
+            dificultad
+    )
+        .then((res) => res.json())
+        .then((result) => {
+            return result.data;
+        })
+        .catch((error) => {
+            console.log("error", error);
+        });
+};
--- src/Views/ComprensionLectora/index.js
+++ src/Views/ComprensionLectora/index.js
@@ -1,215 +1,233 @@
 // Librerias
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
-  Container,
-  makeStyles,
-  Paper,
-  Typography,
-  Button,
-  Grid,
+    Container,
+    makeStyles,
+    Paper,
+    Typography,
+    Button,
+    Grid,
 } from "@material-ui/core";
 
 // Componentes externos
-import LayaoutGame from '../../Components/Layout/LayaoutContainer';
+import LayaoutGame from "../../Components/Layout/LayaoutContainer";
 
 // Componentes internos
 import * as BaseStyle from "./Styles";
-import controller from "./Controller";
+import { obtenerNivel } from "./Controller";
 import { Background } from "./Assets";
-import {authFetch} from "../../AuthProvider"
+import { authFetch } from "../../AuthProvider";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-  },
-  containerOpciones: {
-    /* MOBILE */
-    alignSelf: "strech",
-    justifyContent: "center",
-    padding: ".1rem",
-    display: "flex",
-    label: {
-      color: "white",
-      height: "12vw",
-      width: "12vw",
-      //SCALE FOR TABLET
-      "@media (min-width: 768px)": {
-        height: "8vw",
-        width: "8vw",
-      },
+    root: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
     },
-    flexDirection: "column",
-    /* TABLET */
-    "@media (min-width: 768px)": {
-      fontSize: "2rem",
-      padding: "2rem",
+    containerOpciones: {
+        /* MOBILE */
+        alignSelf: "strech",
+        justifyContent: "center",
+        padding: ".1rem",
+        display: "flex",
+        label: {
+            color: "white",
+            height: "12vw",
+            width: "12vw",
+            //SCALE FOR TABLET
+            "@media (min-width: 768px)": {
+                height: "8vw",
+                width: "8vw",
+            },
+        },
+        flexDirection: "column",
+        /* TABLET */
+        "@media (min-width: 768px)": {
+            fontSize: "2rem",
+            padding: "2rem",
+        },
+        "@media (min-width: 1000px)": {
+            fontSize: "6rem",
+            padding: "2rem",
+            alignSelf: "Center",
+            textAlign: "center",
+            maxWidth: "900px",
+        },
     },
-    "@media (min-width: 1000px)": {
-      fontSize: "6rem",
-      padding: "2rem",
-      alignSelf: "Center",
-      textAlign: "center",
-      maxWidth: "900px",
+    container: BaseStyle.ContainerCenter,
+    H1: BaseStyle.TitleH1,
+    H2: {
+        fontSize: "1.5rem",
+        padding: ".7rem",
+        textAlign: "center",
+        "@media (min-width: 768px)": {
+            fontSize: "2rem",
+            padding: ".5rem",
+        },
     },
-  },
-  container: BaseStyle.ContainerCenter,
-  H1: BaseStyle.TitleH1,
-  H2: {
-    fontSize: "1.5rem",
-    padding: ".7rem",
-    textAlign: "center",
-    "@media (min-width: 768px)": {
-      fontSize: "2rem",
-      padding: ".5rem",
+    H3: {
+        fontSize: "1.5rem",
+        padding: ".5rem",
+        color: "white",
+        textAlign: "center",
+        textShadow: "1px 1px 1px black",
+        "@media (min-width: 768px)": {
+            fontSize: "3rem",
+            padding: "1rem",
+        },
     },
-  },
-  H3: {
-    fontSize: "1.5rem",
-    padding: ".5rem",
-    color: "white",
-    textAlign: "center",
-    textShadow: "1px 1px 1px black",
-    "@media (min-width: 768px)": {
-      fontSize: "3rem",
-      padding: "1rem",
-    },
-  },
-  paper: BaseStyle.paper,
-  paperCorrecta: BaseStyle.paperCorrecta,
-  paperIncorrecta: BaseStyle.paperIncorrecta,
-  paperNumero: BaseStyle.paperNumero,
+    paper: BaseStyle.paper,
+    paperCorrecta: BaseStyle.paperCorrecta,
+    paperIncorrecta: BaseStyle.paperIncorrecta,
+    paperNumero: BaseStyle.paperNumero,
 }));
 
 const ComprensionLectora = (props) => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  const [nivelState, setNivelState] = useState({
-    dificultad: 1,
-    nivel: controller.obtenerNivel(1),
-    perdio: false,
-  });
-
-  //Layout hook
-  const [puntos, setPuntos] = useState(0);
-  const [winner, setWinner] = useState(false);
-  const [loser, setLoser] = useState(false);
-
-  const [gameState, setGameState] = useState({
-    perdio: false,
-  });
-
-  const opcionClickHandler = (opcion) => {
-    if (opcion.Correcta === true) {
-      console.log("Correcta!");
-      subirNivel();
-    } else {
-      setLoser(true);
-      console.log("Incorrecta");
-    }
-  };
-
-  const subirNivel = () => {
-    getPoints();
-    if (nivelState.dificultad === 3) {
-      //alert("ganaste!");
-      //EL juego termino.     
-      setWinner(true);
-      return;
-    }
-
-    var nuevaDificultad = nivelState.dificultad + 1;
-    setNivelState({
-      dificultad: nuevaDificultad,
-      nivel: controller.obtenerNivel(nuevaDificultad),
+    const [nivelState, setNivelState] = useState({
+        dificultad: 1,
+        nivel: null,
+        perdio: false,
     });
-  };
-  
-  const getPoints = () => {
-		authFetch("https://backendlenguamaticag1.herokuapp.com/api/player/levelUp?game=comprensionLectora&level=" + nivelState.dificultad)
-			.then(response => response.json())
-			.then(json => {
-				console.log(json)
-			})
-			.catch(error => console.log('error', error));
-	}
 
-  const reiniciar = () => {
-    setGameState({ perdio: false });
+    //Layout hook
+    const [puntos, setPuntos] = useState(0);
+    const [winner, setWinner] = useState(false);
+    const [loser, setLoser] = useState(false);
+    const [loading, setLoading] = useState(true);
 
-    setNivelState({
-      dificultad: 1,
-      nivel: controller.obtenerNivel(1),
+    const [gameState, setGameState] = useState({
+        perdio: false,
     });
-  };
 
-  return (
-    <LayaoutGame 
-      level={nivelState.dificultad}
-      points={puntos}
-      winner={winner}
-      loser={loser}
-      game="CompresionLectora"
-      backgroundImage={Background}
-    >
-      <div className={classes.root}>
-        <Container className={classes.container}>
-          <Grid container>
-            <Grid item xs={12}>
-              <Typography className={classes.H3}>
-                <Paper className={classes.paperNumero}>
-                  {"Nivel " + nivelState.dificultad}
-                </Paper>
-              </Typography>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Typography className={classes.H2}>
-                <Paper className={classes.paperNumero}>
-                  {nivelState.nivel.Pregunta}
-                </Paper>
-              </Typography>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Container className={classes.containerOpciones}>
-                {nivelState.nivel.Respuestas.map((opcion) => (
-                  <Typography className={classes.H3}>
-                    <Paper
-                      elevation={4}
-                      style={{ marginBottom: "0px" }}
-                      className={
-                        !gameState.perdio
-                          ? classes.paper
-                          : opcion.Correcta
-                          ? classes.paperCorrecta
-                          : classes.paperIncorrecta
-                      }
-                      onClick={() =>
-                        gameState.perdio ? null : opcionClickHandler(opcion)
-                      }
-                      key={opcion.Descripcion}
-                    >
-                      {opcion.Descripcion}
-                    </Paper>
-                  </Typography>
-                ))}
-              </Container>
-            </Grid>
-          </Grid>
-          {gameState.perdio ? (
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={() => reiniciar()}
-            >
-              Jugar de nuevo
-            </Button>
-          ) : null}
-        </Container>
-      </div>
-    </LayaoutGame>
-  );
+    useEffect(() => {
+        recuperarNivel(1);
+    }, []);
+
+    const recuperarNivel = (dificultad) => {
+        setLoading(true);
+        var promise = obtenerNivel(dificultad);
+        promise.then((data) => {
+            setNivelState({
+                dificultad: dificultad,
+                nivel: data,
+                perdio: false,
+            });
+            setGameState({ perdio: false });
+            setLoading(false);
+        });
+    };
+
+    const opcionClickHandler = (opcion) => {
+        if (opcion.correcta === true) {
+            console.log("Correcta!");
+            subirNivel();
+        } else {
+            setLoser(true);
+            console.log("Incorrecta");
+        }
+    };
+
+    const subirNivel = () => {
+        getPoints();
+        if (nivelState.dificultad === 3) {
+            //EL juego termino.
+            setWinner(true);
+            return;
+        }
+
+        var nuevaDificultad = nivelState.dificultad + 1;
+        recuperarNivel(nuevaDificultad);
+    };
+
+    const getPoints = () => {
+        authFetch(
+            "https://backendlenguamaticag1.herokuapp.com/api/player/levelUp?game=comprensionLectora&level=" +
+                nivelState.dificultad
+        )
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json);
+            })
+            .catch((error) => console.log("error", error));
+    };
+
+    const reiniciar = () => {
+        recuperarNivel(1);
+    };
+
+    return loading ? (
+        <CircularProgress />
+    ) : (
+        <LayaoutGame
+            level={nivelState.dificultad}
+            points={puntos}
+            winner={winner}
+            loser={loser}
+            game="CompresionLectora"
+            backgroundImage={Background}
+        >
+            <div className={classes.root}>
+                <Container className={classes.container}>
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <Typography className={classes.H3}>
+                                <Paper className={classes.paperNumero}>
+                                    {"Nivel " + nivelState.dificultad}
+                                </Paper>
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <Typography className={classes.H2}>
+                                <Paper className={classes.paperNumero}>
+                                    {nivelState.nivel.pregunta}
+                                </Paper>
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <Container className={classes.containerOpciones}>
+                                {nivelState.nivel.respuestas.map((opcion) => (
+                                    <Typography className={classes.H3}>
+                                        <Paper
+                                            elevation={4}
+                                            style={{ marginBottom: "0px" }}
+                                            className={
+                                                !gameState.perdio
+                                                    ? classes.paper
+                                                    : opcion.correcta
+                                                    ? classes.paperCorrecta
+                                                    : classes.paperIncorrecta
+                                            }
+                                            onClick={() =>
+                                                gameState.perdio
+                                                    ? null
+                                                    : opcionClickHandler(opcion)
+                                            }
+                                            key={opcion.descripcion}
+                                        >
+                                            {opcion.descripcion}
+                                        </Paper>
+                                    </Typography>
+                                ))}
+                            </Container>
+                        </Grid>
+                    </Grid>
+                    {gameState.perdio ? (
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            onClick={() => reiniciar()}
+                        >
+                            Jugar de nuevo
+                        </Button>
+                    ) : null}
+                </Container>
+            </div>
+        </LayaoutGame>
+    );
 };
 
 export default ComprensionLectora;
